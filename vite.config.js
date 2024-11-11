09767c299e881f726141de38839d4c0e783d6206
PRABHAKAR V <prabhakar15v@gmail.com>
2024-11-11 21:43:35 +0530

    Commit Message

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Akilraj_N-Portfolio",
+  base: "/PRABHAKAR_V_PORTFOLIO",
   plugins: [react()],
 })
