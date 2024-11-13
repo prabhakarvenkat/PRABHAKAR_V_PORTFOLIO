4ec92a4e1417e425c63b89c166d70fa9d362e702
PRABHAKAR V <117185272+prabhakarvenkat@users.noreply.github.com>
2024-11-13 21:43:24 +0530

    Update Profile.jsx

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -78,14 +78,15 @@ const Profile = () => {
           className="greetingtext text-xl font-semibold"
           variants={itemVariants}
         >
-          Hello, I am
+          Hello, I am <br></br>
+          <b>PRABHAKAR V</b>
         </motion.h2>
-        <motion.div
+        {/* <motion.div
           className="logo py-2 px-4 flex justify-center"
           variants={itemVariants}
         >
           <Name isDarkTheme={isDarkTheme} height="h-20" />
-        </motion.div>
+        </motion.div> */}
       </div>
 
       <motion.p
